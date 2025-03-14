8dd21098803babcf6908d1c0fff0ed68779b7bba
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2025-03-14 20:57:37 +0600

    Added sonnet

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,7 +1,7 @@
-
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "./ThemeProvider";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -10,9 +10,19 @@ export function ThemeToggle() {
     <Button
       variant="ghost"
       size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="rounded-full"
-    >
+      onClick={() => {
+        setTheme(theme === "light" ? "dark" : "light");
+        theme === "dark" &&
+          toast.success(
+            <div style={{ display: "justify-center", alignItems: "center" }}>
+              <img src="/my-eyes.gif" alt="My eyes" style={{ width: "100%" }} />
+            </div>,
+            {
+              style: { width: "200px" },
+            }
+          );
+      }}
+      className="rounded-full">
       {theme === "light" ? (
         <Moon className="h-5 w-5" />
       ) : (
